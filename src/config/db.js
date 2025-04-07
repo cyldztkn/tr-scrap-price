ac2d87044e90966d88ae56d6608aeba72ba64c66
Cagkan <cagkanyildiztekin14@gmail.com>
2025-04-07 17:28:00 +0300

    "deploy"

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,15 @@
+import { MongoClient } from "mongodb"; // MongoDB Client'ı import et
 import mongoose from 'mongoose';
 import config from './config.js';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(config.mongodbUri);
-        console.log('MongoDB Connected...');
-    } catch (err) {
-        console.error('MongoDB connection error:', err);
-        process.exit(1);
-    }
+  try {
+      await mongoose.connect(config.mongodbUri);
+      console.log('MongoDB Connected...');
+  } catch (err) {
+      console.error('MongoDB connection error:', err);
+      process.exit(1);
+  }
 };
 
-export default connectDB; 
+export default connectDB;
